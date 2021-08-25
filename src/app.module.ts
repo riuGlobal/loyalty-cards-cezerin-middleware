9a02169274f30db130b7027ecbf2e42c89dbe070
ricardo <ricardo.david.rd@gmail.com>
2021-08-24 21:30:13 -0500

    Added typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PunchersPurchasePunchRequestModule } from './models/punchers-purchase-punch-request/punchers-purchase-punch-request.module';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(), PunchersPurchasePunchRequestModule],
   controllers: [AppController],
   providers: [AppService],
 })
